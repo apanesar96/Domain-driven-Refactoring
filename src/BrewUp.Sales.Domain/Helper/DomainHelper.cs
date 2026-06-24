a2c5814f929a4f6bbd292536d282c2c333a079fd
amanpanesar <aman.panesar5@gmail.com>
2026-06-24 21:36:13 +0100

    WIP begin to swap out the read model

--- src/BrewUp.Sales.Domain/Helper/DomainHelper.cs
+++ src/BrewUp.Sales.Domain/Helper/DomainHelper.cs
@@ -6,14 +6,11 @@ namespace BrewUp.Sales.Domain.Helper;
 
 public static class DomainHelper
 {
-    internal static IEnumerable<SalesOrderRow> MapToDomainRows(this IEnumerable<SalesOrderRowJson> json)
-    {
-        return json.Select(r => SalesOrderRow.CreateSalesOrderRow(new BeerId(r.BeerId), new BeerName(r.BeerName), r.Quantity, r.Price));
-    }
+    internal static IEnumerable<SalesOrderRow> MapToDomainRows(this IEnumerable<SalesOrderRowJson> json) => 
+        json.Select(r => SalesOrderRow.CreateSalesOrderRow(new BeerId(r.BeerId), new BeerName(r.BeerName), r.Quantity, r.Price));
 
-    internal static Shared.Entities.SalesOrder MapToSharedDto(this SalesOrder salesOrder)
-    {
-        return Shared.Entities.SalesOrder.Create(salesOrder._salesOrderId, salesOrder._salesOrderNumber,
+    internal static SalesOrder MapToSharedDto(this SalesOrder salesOrder) =>
+        SalesOrder.CreateSalesOrder(salesOrder._salesOrderId, salesOrder._salesOrderNumber,
             salesOrder._orderDate, salesOrder._customerId, salesOrder._customerName,
             salesOrder._rows.Select(r => new SalesOrderRowJson
             {
@@ -21,6 +18,5 @@ public static class DomainHelper
                 BeerName = r._beerName.Value,
                 Quantity = r._quantity,
                 Price = r._beerPrice
-            }));    
-    }
+            }));
 }
--- src/BrewUp.Warehouses.Domain/Helper/DomainHelper.cs
+++ src/BrewUp.Warehouses.Domain/Helper/DomainHelper.cs
@@ -1,11 +1,11 @@
-﻿using BrewUp.Warehouses.Domain.Entities;
+﻿
+
+using BrewUp.Shared.Entities;
 
 namespace BrewUp.Warehouses.Domain.Helper;
 
 public static class DomainHelper
 {
-	internal static Shared.Entities.Availability MapToSharedDto(this Availability availability)
-	{
-		return Shared.Entities.Availability.Create(availability._beerId, availability._beerName, availability._quantity);
-	}
+	internal static Availability MapToSharedDto(this Entities.Availability availability) => 
+		Availability.Create(availability._beerId, availability._beerName, availability._quantity);
 }
